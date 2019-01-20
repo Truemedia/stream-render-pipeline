5415957055684bec2510883300bba25d5df5e77a
Wade Penistone <wadespackages@yahoo.com>
2019-01-20 14:28:26 +0000

    Helper functions@

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
+const lazypipe = require('lazypipe');
+const gulpPlugins = require('auto-plug')('gulp');
+
 class Srp
 {
   constructor(opts)
   {
     this.opts = opts;
+  }
+
+  /**
+    * Files
+    */
+  src(glob)
+  {
+    return lazypipe().pipe(gulpPlugins.addSrc, this.tplPath(__dirname, glob))
+  }
+
+  /**
+    * Templating
+    */
+  tpl(data)
+  {
+    return lazypipe().pipe(gulpPlugins.template, data);
+  }
+
+  /**
+    * Rename files and folders
+    */
+  get rename()
+  {
+    return gulpPlugins.rename;
   }
 
   tplPath(cwd, relativePath)
